ea3fb8056e4967e48fb995da51460500db9ebe1e
Peter Macko <petermacko90@gmail.com>
2018-04-08 11:40:35 +0200

    hiding api key to env var

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-  apiKey: 'e82bbe3eb91b4998b6b39ff78703ebed'
+  apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
@@ -9,7 +9,7 @@ const handleApiCall = (req, res) => {
 		.then(data => {
 			res.json(data);
 		})
-		.catch(err => res.status(400).json('unable to work with api'))
+		.catch(err => res.status(400).json('unable to work with api'));
 }
 
 const handleImage = (req, res, db) => {
@@ -20,10 +20,10 @@ const handleImage = (req, res, db) => {
 		.then(entries => {
 			res.json(entries[0]);
 		})
-		.catch(err => res.status(400).json('unable to get entries'))
+		.catch(err => res.status(400).json('unable to get entries'));
 }
 
 module.exports = {
 	handleImage,
 	handleApiCall
-}
+};
